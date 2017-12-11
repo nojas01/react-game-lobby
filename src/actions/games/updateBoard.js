082dcfce1ed4728e0dfed921d0a5e42fd9e9e170
nojas01 <j.r.nede@gmail.com>
2017-12-11 14:25:31 +0100

    patch works but no payload

--- src/actions/games/updateBoard.js
+++ src/actions/games/updateBoard.js
@@ -1,15 +1,21 @@
-// import API from '../../api/client'
+import API from '../../api/client'
+// import game from '../../containers/Game'
 
 export const UPDATE_BOARD = 'UPDATE_BOARD'
 
-// const api = new API()
+const api = new API()
 
-export default index => (dispatch) => () => {
+export default (index, game) => (dispatch) => () => {
   dispatch ({
     type: 'UPDATE_BOARD',
     payload: index
   })
 
+  api.patch(`/games/${game._id}`, {index})
+      .then((res) => {
+          console.log(res);
+          console.log(index);
+      })
 
 }
 
